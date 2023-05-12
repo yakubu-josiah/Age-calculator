7135bf3f8189fe14e80cbd536fff648dbbf567b1
yakubu-josiah <jossysmothest007@gmail.com>
2023-05-12 18:55:03 +0100

    simplifying checkLeapYear code & testing in console

--- index.js
+++ index.js
@@ -3,27 +3,15 @@ const buttonEl = document.getElementById("btn");
 const presentDate = new Date();
 
 function checkLeapYear(year) {
-  if (year % 4 === 0) {
-    if (year % 100 === 0) {
-      if (year % 400 === 0) {
-        return true;
-      } else {
-        return false;
-      }
-    } else {
-      return true;
-    }
-  } else {
-    return false;
-  }
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 }
+
 buttonEl.addEventListener("click", function () {
   console.log("Clicked!!");
   let birthDay = inputEl.value;
-  // console.log(Date(birthDay).getFullYear());
-  var birthYear = new Date(birthDay).getFullYear();
-  var birthMonth = new Date(birthDay).getMonth() + 1;
-  var birthDate = new Date(birthDay).getDate();
+  let birthYear = new Date(birthDay).getFullYear();
+  let birthMonth = new Date(birthDay).getMonth() + 1;
+  let birthDate = new Date(birthDay).getDate();
   console.log("Your birth date is " + birthDate);
   console.log("Your birth month is " + birthMonth);
   console.log("Your birth year was in " + birthYear);
@@ -32,7 +20,7 @@ buttonEl.addEventListener("click", function () {
   let result = presentDate.getFullYear() - birthYear;
   result--;
   console.log(result);
-  var leapBirth = checkLeapYear(birthYear); 
+  let leapBirth = checkLeapYear(birthYear); 
   console.log(leapBirth +  `, it's ${leapBirth == 1 ? "a" : "not a"} leap year`);
   // console.log(checkLeapYear(presentDate.getFullYear()));
 });
